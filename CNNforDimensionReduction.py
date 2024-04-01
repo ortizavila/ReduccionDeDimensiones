211070ff82bf23f84258bbc9cc474c3f386db57f
Alfonso Ortiz Avila <130321366+ortizavila@users.noreply.github.com>
2024-04-01 13:24:52 -0600

    Update CNNforDimensionReduction.py

--- CNNforDimensionReduction.py
+++ CNNforDimensionReduction.py
@@ -1,4 +1,39 @@
 #### Importar modelo entrenado con data augmentation e implementado para GradCAM
 #### Removemos las últimas capas que no son convolucionales, para dejar sólo las capas
-##### encargadas de realizar la reducción de dimensiones mediante 
+##### encargadas de realizar la reducción de dimensiones mediante la aplicación de filtros de 
+###   convolution, strides y maxpooling
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+### Importar modelo entrenado, con el que hemos estado haciendo inferencias
+
+
+
+model = keras.models.load_model('.../DataAugmentation/modelo_para_GradCAM2')
+
+
+
+##### --->>>> Retirar capas que no son convolucionales y usar la reducción de dimensiones dada por las capas 
+#### de convolución.
+
+
+
+
+model2 = keras.Sequential()
+
+for layer in model.layers[:-3]: # remover 
+    model2.add(layer)    
+
+
+# Freeze layers ### remover la posibilidad de seguir entrenando las capas. 
+for layer in model2.layers:
+    layer.trainable = False
+
+
+
 
