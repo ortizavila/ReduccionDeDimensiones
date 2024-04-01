f2040b7a8c15fcc8d6263682fd79209a02723efd
Alfonso Ortiz Avila <130321366+ortizavila@users.noreply.github.com>
2024-04-01 13:12:59 -0600

    Create CNNforDimensionReduction.py

---
+++ CNNforDimensionReduction.py
@@ -0,0 +1,4 @@
+#### Importar modelo entrenado con data augmentation e implementado para GradCAM
+#### Removemos las últimas capas que no son convolucionales, para dejar sólo las capas
+##### encargadas de realizar la reducción de dimensiones mediante 
+
